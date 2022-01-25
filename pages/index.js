3f221d927a9a62f0317bdf1554e83d70446caf1c
Alam <alamkhalid0803@gmail.com>
2022-01-25 20:26:26 +0500

    google translate

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Banner from '../components/HomeOne/Banner';
-import SingleTour from '../components/HomeOne/SingleTour';
-import Promo from '../components/HomeOne/Promo';
-import AdventureTour from '../components/HomeOne/AdventureTour';
-import BestTour from '../components/HomeOne/BestTour';
-import RecommendTour from '../components/HomeOne/RecommendTour';
-import Service from '../components/HomeOne/Service';
-import Quote from '../components/HomeOne/Quote';
-import Expertise from '../components/HomeOne/Expertise';
-import Partner from '../components/HomeOne/Partner';
-import Destination from '../components/HomeOne/Destination';
-import Specialties from '../components/HomeOne/Specialties';
-import FeatureTour from '../components/HomeOne/FeatureTour';
-import About from '../components/HomeOne/About';
-import Product from '../components/common/Product';
-import Newsletter from '../components/common/Newsletter';
-import Blog from '../components/HomeOne/Blog';
+import React from "react";
+import Banner from "../components/HomeOne/Banner";
+import SingleTour from "../components/HomeOne/SingleTour";
+import Promo from "../components/HomeOne/Promo";
+import AdventureTour from "../components/HomeOne/AdventureTour";
+import BestTour from "../components/HomeOne/BestTour";
+import RecommendTour from "../components/HomeOne/RecommendTour";
+import Service from "../components/HomeOne/Service";
+import Quote from "../components/HomeOne/Quote";
+import Expertise from "../components/HomeOne/Expertise";
+import Partner from "../components/HomeOne/Partner";
+import Destination from "../components/HomeOne/Destination";
+import Specialties from "../components/HomeOne/Specialties";
+import FeatureTour from "../components/HomeOne/FeatureTour";
+import About from "../components/HomeOne/About";
+import Product from "../components/common/Product";
+import Newsletter from "../components/common/Newsletter";
+import Blog from "../components/HomeOne/Blog";
 
 const index = () => {
   return (
@@ -23,39 +23,37 @@ const index = () => {
       <Banner />
 
       <Quote
-        imgOne='/images/service/service-1.png'
-        imgTwo='/images/service/service-2.png'
-        imgThree='/images/service/service-3.png'
-        imgFour='/images/service/service-4.png'
+        imgOne="/images/service/service-1.png"
+        imgTwo="/images/service/service-2.png"
+        imgThree="/images/service/service-3.png"
+        imgFour="/images/service/service-4.png"
       />
 
       <Expertise />
 
       <Partner
-        partner1='/images/partners/partners-1.png'
-        partner2='/images/partners/partners-2.png'
-        partner3='/images/partners/partners-3.jpeg'
-        partner4='/images/partners/partners-4.png'
-        partner5='/images/partners/partners-5.png'
+        partner1="/images/partners/partners-1.png"
+        partner2="/images/partners/partners-2.png"
+        partner3="/images/partners/partners-3.jpeg"
+        partner4="/images/partners/partners-4.png"
+        partner5="/images/partners/partners-5.png"
       />
 
       <RecommendTour
-        des1='india'
-        des2='germany'
-        des3='thailand'
-        des4='malaysia'
-        des5='canada'
+        des1="india"
+        des2="germany"
+        des3="thailand"
+        des4="malaysia"
+        des5="canada"
       />
 
-      <Specialties 
-        spec1='/images/team/team-1.jpg'
-        spec2='/images/team/team-2.jpg'
-        spec3='/images/team/team-3.jpg'
-        spec4='/images/team/team-4.jpg'
-        spec5='/images/team/team-5.jpg'
+      <Specialties
+        spec1="/images/team/team-1.jpg"
+        spec2="/images/team/team-2.jpg"
+        spec3="/images/team/team-3.jpg"
+        spec4="/images/team/team-4.jpg"
+        spec5="/images/team/team-5.jpg"
       />
-
-
     </>
   );
 };
