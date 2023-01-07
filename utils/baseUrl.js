1df198ddca4e1e51e49da6acae408eed3234c657
sachin <sachinsapkota773@gmail.com>
2023-01-07 12:40:32 +0545

    added lock file

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,6 @@
-const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://detour-react.hibootstrap.com' 
-: 'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://hospatal.com'
+    : 'http://localhost:3000';
 
 export default baseUrl;
